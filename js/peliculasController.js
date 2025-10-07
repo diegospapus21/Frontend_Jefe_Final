5d6585519a2a80bdfc8fe926e2e6564c1a30eaab
diegospapus21 <diegoghc7002@gmail.com>
2025-10-07 11:24:34 -0600

    ajein

---
+++ js/peliculasController.js
@@ -0,0 +1,39 @@
+import { getPelicula, createPelicula, updatePelicula, deletePelicula } from "./peliculasService";
+
+export async function loadPeliculas(tbody) {
+    const pelicula = await getPelicula();
+    tbody.innerHTML= "";
+    pelicula.forEach(P => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td>${p.id}</td><td>${p.Titulo}</td><td>${p.Director}</td><td>${p.Genero}</td>
+        <td>${p.Estreno}</td><td>${p.Año}</td><td>${p.Duracion}</td><td>${p.Creacion}</td><td><button data id="${P.id}" class="edit">Editar</button>
+        <button data id="${P.id}" class="delete">Borrar</button></td>`;
+        tbody.appendchild(tr);
+    });
+}
+
+export async function bindPeliculaForm(Form, tbody) {
+    Form.addEventListener('submit', async (e)=> {
+        e.preventDefault();
+        const fd = new FormData(form);
+        const dto = {Titulo:fd.get('Titulo'), Director:fd.get('Director'), Genero:fd.get('Genero'), Año:fd.get('Año'), Duracion:fd.get('Duracion') ? Number(fd.get('Duracion')):null, Fecha:fd.get('Fecha')};
+        await createPelicula(dto);
+        form.reset();
+        await loadPeliculas(tbody);
+    });
+}
+
+export function bindTableActions(tbody, modalCallback){
+    tbody.addEventListener('click', async (e)=> {
+        if(e.target.matches('.delete')){
+            const id = e.target.dataset.id;
+            if(confirm('Borrar Pelicula'+ id + '?')){
+                await deletePelicula(id);
+                await loadPeliculas(tbody);
+            }
+        }else if (e.target.matches('.edit')){
+            const id = e.target.dataset.id;
+            modalCallback(id);
+        }
+    });
+}
--- js/peliculsController.js
+++
@@ -1,5 +0,0 @@
-import { getPelicula, createPelicula, updatePelicula, deletePelicula } from "./peliculasService";
-
-export async function loadPeliculas(tbody) {
-    const pelicula = await getPelicula();
-}
